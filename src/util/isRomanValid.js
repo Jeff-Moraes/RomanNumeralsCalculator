6d8bf749e235a28782dac49ed16f6bef22f11cdc
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-02-06 16:59:01 +0100

    Add validation - number repetition

--- src/util/isRomanValid.js
+++ src/util/isRomanValid.js
@@ -9,17 +9,24 @@ export default function isRomanValid(romanArray, numbersObject) {
       if (romanArray.indexOf(value) !== romanArray.lastIndexOf(value)) {
         validRoman = false;
 
-        console.log("contem mais de um V L D");
+        console.log("more than one V L D");
       }
     } else if (
       arr[index + 1] &&
       !numbersObject[value].before.includes(arr[index + 1])
     ) {
       validRoman = false;
+    } else if (arr.filter(i => i === value).length > 3) {
+      validRoman = false;
+      console.log(arr.filter(i => i === value).length, "COUNT");
+    } else if (arr[index - 2] === value) {
+      validRoman = false;
+      console.log("Same number", value);
     }
+    console.log(arr[index + 1], "=", arr[index - 1]);
   });
 
   console.log("validRoman", validRoman);
 
-  return validRoman ? true : false;
+  return validRoman;
 }
